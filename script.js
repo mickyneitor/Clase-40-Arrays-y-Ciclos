91324333fc4d7bc3ef26294642a23a83f4314abf
Micaela Doña <imd.mica@gmail.com>
2021-04-26 19:40:35 -0300

    data

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Arrays
+// nombreArray = []
+
+const vacio = [];
+const frutas = ["manzana", "platano", "pera", "mango"];
+const edades = [1, 2, 3, 4, 5, 10, 8];
+const booleanos = [true, true, false, true];
+
+// "manzana" == frutas[0]
+
+// Acceder a los valores de un array
+// array[posicionElemento]
+
+//console.log(frutas[2])
+
+
+// Array tienen la propiedad length (Inicia en uno) (Longitud del arreglo)
+
+console.log(vacio.length)
+
+const ultimoElemento = frutas[frutas.length]
+console.log(ultimoElemento)
